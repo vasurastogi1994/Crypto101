ed00b1b706402a4d135308ffda836cb24675fec4
Vasu Rastogi <vasurastogi1994@gmail.com>
2024-08-03 19:45:14 +0530

    Some changes

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const crypto = require('crypto');
 
-const input = "mytest";
-const hash = crypto.createHash('sha256').update(input).digest('hex');
 
-console.log(hash)
+
+function findHash(pattern){
+    let number = 0;
+    do{
+    const hash = crypto.createHash('sha256').update('vrastogi' + number.toString()).digest('hex');
+    if(hash.startsWith(pattern)){
+        break;
+    }
+    number++;
+    }
+    while(true)
+        return number;
+    }
+
+
+console.log(findHash('00000'))
